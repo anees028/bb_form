8aa19421fdfcb390ede1c3ab0ce70ae0f91c6a9d
anees50hertz <anees50hertz@gmail.com>
2023-09-14 00:17:10 +0200

    Form prepare Navbar is pending. component not found on nova.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import './App.css'
 import UserForm from './components/form/form';
+import Navbar from './components/navbar/navbar';
 
 function App() {
   
   return (
-    <>
+    <div>
+      <Navbar />
+      <hr></hr>
       <UserForm />
-    </>
+    </div>
+
+
   )
 }
 
